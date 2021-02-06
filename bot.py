869087980508d3ac0f45b7998b288d0c38bb436c
Kandarp-Devmurari <75673068+Kandarp-Devmurari@users.noreply.github.com>
2021-02-06 20:06:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,7 @@ import discord
 from discord.ext import commands
 import random
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-There are a number of utility commands being showcased here.'''
-
+description = '''A bot that allows the player to play a rpg maze game'''
 intents = discord.Intents.default()
 intents.members = True
 
